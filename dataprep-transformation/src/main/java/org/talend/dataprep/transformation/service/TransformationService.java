e1fc86e42151369119a00d316a630b78e1584314
Vincent Jaouën <vjaouen@talend.com>
2016-12-14 16:09:02 +0100

    fature(TDP-2952): [backend] add API endpoints to get columns semantic types :  ->  /api/datasets/{datasetId}/columns/{columnId}/types for datasets  -> /api/preparations/{preparationId}/columns/{columnId}/types for preparations

--- dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/TransformationService.java
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/TransformationService.java
@@ -13,12 +13,16 @@
 
 package org.talend.dataprep.transformation.service;
 
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 import static org.talend.daikon.exception.ExceptionContext.build;
 import static org.talend.dataprep.api.export.ExportParameters.SourceType.HEAD;
+import static org.talend.dataprep.exception.error.PreparationErrorCodes.PREPARATION_DOES_NOT_EXIST;
+import static org.talend.dataprep.exception.error.TransformationErrorCodes.UNEXPECTED_EXCEPTION;
+import static org.talend.dataprep.quality.AnalyzerService.Analysis.SEMANTIC;
 import static org.talend.dataprep.transformation.actions.category.ScopeCategory.COLUMN;
 import static org.talend.dataprep.transformation.actions.category.ScopeCategory.LINE;
 import static org.talend.dataprep.transformation.format.JsonFormat.JSON;
@@ -32,6 +36,7 @@ import java.util.zip.GZIPOutputStream;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
+import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.lang.StringUtils;
 import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
@@ -49,14 +54,18 @@ import org.talend.dataprep.api.dataset.DataSet;
 import org.talend.dataprep.api.dataset.DataSetMetadata;
 import org.talend.dataprep.api.dataset.RowMetadata;
 import org.talend.dataprep.api.dataset.row.Flag;
+import org.talend.dataprep.api.dataset.statistics.SemanticDomain;
 import org.talend.dataprep.api.export.ExportParameters;
 import org.talend.dataprep.api.preparation.Action;
+import org.talend.dataprep.api.preparation.Preparation;
 import org.talend.dataprep.api.preparation.Step;
 import org.talend.dataprep.api.preparation.StepDiff;
 import org.talend.dataprep.cache.ContentCache;
 import org.talend.dataprep.cache.ContentCacheKey;
 import org.talend.dataprep.command.dataset.DataSetGet;
 import org.talend.dataprep.command.dataset.DataSetGetMetadata;
+import org.talend.dataprep.command.preparation.PreparationDetailsGet;
+import org.talend.dataprep.dataset.StatisticsAdapter;
 import org.talend.dataprep.exception.TDPException;
 import org.talend.dataprep.exception.error.CommonErrorCodes;
 import org.talend.dataprep.exception.error.TransformationErrorCodes;
@@ -84,6 +93,8 @@ import org.talend.dataprep.transformation.cache.CacheKeyGenerator;
 import org.talend.dataprep.transformation.cache.TransformationMetadataCacheKey;
 import org.talend.dataprep.transformation.pipeline.ActionRegistry;
 import org.talend.dataprep.transformation.preview.api.PreviewParameters;
+import org.talend.dataquality.common.inference.Analyzer;
+import org.talend.dataquality.common.inference.Analyzers;
 import org.talend.dataquality.semantic.broadcast.BroadcastIndexObject;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -229,8 +240,8 @@ public class TransformationService extends BaseTransformationService {
      * @param rawParams the aggregation rawParams as body rawParams.
      */
     // @formatter:off
-    @RequestMapping(value = "/aggregate", method = POST, produces = APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Compute the aggregation according to the request body rawParams", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/aggregate", method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Compute the aggregation according to the request body rawParams", consumes = APPLICATION_JSON_VALUE)
     @VolumeMetered
     public AggregationResult aggregate(@ApiParam(value = "The aggregation rawParams in json") @RequestBody final String rawParams) {
         // @formatter:on
@@ -424,8 +435,8 @@ public class TransformationService extends BaseTransformationService {
      * A diff is between 2 sets of actions and return the info like created columns ids
      */
     //@formatter:off
-    @RequestMapping(value = "/transform/diff/metadata", method = POST, produces = APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Given a list of requested preview, it applies the diff to each one. A diff is between 2 sets of actions and return the info like created columns ids", notes = "This operation returns the diff metadata", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/transform/diff/metadata", method = POST, produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Given a list of requested preview, it applies the diff to each one. A diff is between 2 sets of actions and return the info like created columns ids", notes = "This operation returns the diff metadata", consumes = APPLICATION_JSON_VALUE)
     @VolumeMetered
     public List<StepDiff> getCreatedColumns(@ApiParam(name = "body", value = "Preview parameters list in json.") @RequestBody final List<PreviewParameters> previewParameters) {
         return previewParameters.stream().map(this::getCreatedColumns).collect(toList());
@@ -678,6 +689,110 @@ public class TransformationService extends BaseTransformationService {
                 oos.writeObject(result);
             }
         };
+    }
+
+    /**
+     * Return the semantic types for a given preparation / column.
+     *
+     * @param preparationId the preparation id.
+     * @param columnId the column id.
+     * @param stepId the step id (optional, if not specified, it's 'head')
+     * @return the semantic types for a given preparation / column.
+     */
+    @RequestMapping(value = "/preparations/{preparationId}/columns/{columnId}/types", method = GET, produces = APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "list the types of the wanted column", notes = "This list can be used by user to change the column type.")
+    @Timed
+    @PublicAPI
+    public List<SemanticDomain> getPreparationColumnSemanticCategories(
+            @ApiParam(value = "The preparation id") @PathVariable String preparationId,
+            @ApiParam(value = "The column id") @PathVariable String columnId,
+            @ApiParam(value = "The preparation version") @RequestParam(defaultValue = "head") String stepId) {
+
+        LOG.debug("listing preparation semantic categories for preparation #{} column #{}@{}", preparationId, columnId, stepId);
+
+        // get the preparation
+        final Preparation preparation;
+        try {
+            final PreparationDetailsGet details = applicationContext.getBean(PreparationDetailsGet.class, preparationId);
+            preparation = mapper.readerFor(Preparation.class).readValue(details.execute());
+        } catch (IOException e) {
+            throw new TDPException(PREPARATION_DOES_NOT_EXIST, e, build().put("id", preparationId));
+        }
+
+        // get the step (in case of 'head', the real step id must be found)
+        final String version = StringUtils.equals("head", stepId) ? preparation.getSteps().get(preparation.getSteps().size() - 1)
+                : stepId;
+
+        /*
+         * OK, this one is a bit tricky so pay attention.
+         *
+         * To be able to get the semantic types, the analyzer service needs to run on the result of the preparation.
+         *
+         * The result must be found in the cache, so if the preparation is not cached, the preparation is run so that
+         * it gets cached.
+         *
+         * Then, the analyzer service just gets the data from the cache. That's it.
+         */
+
+        // generate the cache keys for both metadata & content
+        final ContentCacheKey metadataKey = cacheKeyGenerator //
+                .metadataBuilder() //
+                .preparationId(preparationId) //
+                .stepId(version) //
+                .sourceType(HEAD) //
+                .build();
+
+        final ContentCacheKey contentKey = cacheKeyGenerator //
+                .contentBuilder() //
+                .datasetId(preparation.getDataSetId()) //
+                .preparationId(preparationId) //
+                .stepId(version) //
+                .format(JSON) //
+                .sourceType(HEAD) //
+                .build();
+
+        // if the preparation is not cached, let's compute it to have some cache
+        if (!contentCache.has(metadataKey) || !contentCache.has(contentKey)) {
+
+            final ExportParameters exportParameters = new ExportParameters();
+            exportParameters.setPreparationId(preparationId);
+            exportParameters.setExportType("JSON");
+            exportParameters.setStepId(stepId);
+            exportParameters.setDatasetId(preparation.getDataSetId());
+
+            final StreamingResponseBody streamingResponseBody = executeSampleExportStrategy(exportParameters);
+            try {
+                // the result is not important here as it will be cached !
+                streamingResponseBody.writeTo(new NullOutputStream());
+            } catch (IOException e) {
+                throw new TDPException(UNEXPECTED_EXCEPTION, e);
+            }
+        }
+
+        // run the analyzer service on the cached content
+        try {
+            final DataSetMetadata metadata = mapper.readerFor(DataSetMetadata.class).readValue(contentCache.get(metadataKey));
+            final ColumnMetadata columnMetadata = metadata.getRowMetadata().getById(columnId);
+            final Analyzer<Analyzers.Result> analyzer = analyzerService.build(columnMetadata, SEMANTIC);
+            analyzer.init();
+
+            try (final JsonParser parser = mapper.getFactory().createParser(contentCache.get(contentKey))) {
+                final DataSet dataSet = mapper.readerFor(DataSet.class).readValue(parser);
+                dataSet.getRecords() //
+                        .map(r -> r.get(columnId)) //
+                        .forEach(analyzer::analyze);
+                analyzer.end();
+            }
+
+            final List<Analyzers.Result> analyzerResult = analyzer.getResult();
+            final StatisticsAdapter statisticsAdapter = new StatisticsAdapter(40);
+            statisticsAdapter.adapt(singletonList(columnMetadata), analyzerResult);
+            LOG.debug("found {} for preparation #{}, column #{}", columnMetadata.getSemanticDomains(), preparationId, columnId);
+            return columnMetadata.getSemanticDomains();
+
+        } catch (IOException e) {
+            throw new TDPException(UNEXPECTED_EXCEPTION, e);
+        }
     }
 
 }
